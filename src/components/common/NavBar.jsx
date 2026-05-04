3bc7ccba70bee75c07c703b8308262d5da0344f1
inderjit-zerror <inderjit.zerror@gmail.com>
2026-05-04 17:46:51 +0530

    Done +

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -60,21 +60,21 @@ export default function Navbar() {
               <AnimatedTitle text={'Index'} />
               <div className="w-0 h-[1px] absolute bottom-[-12] left-0 transition-all duration-200 ease-out bg-white group-hover:w-full"></div>
             </a> */}
-            <a href="#about" className="relative group  ">
+            <a href="#about" className="relative group  h-fit  ">
               <AnimatedTitle text={'About'} />
               <div className="w-0 h-[1px] AboutLine absolute bottom-[-12] left-0 transition-all duration-200 ease-out bg-black group-hover:w-full"></div>
             </a>
-            <a href="#services" className=" relative group  ">
+            <a href="#services" className=" relative group h-fit  ">
               <AnimatedTitle text={'Expertise'} />
               
               <div className="w-0 h-[1px] ExpertiseLine absolute bottom-[-12] left-0 transition-all duration-200 ease-out bg-black group-hover:w-full"></div>
             </a>
-            <a href="#work" className=" relative group  ">
+            <a href="#work" className=" relative group h-fit  ">
               <AnimatedTitle text={'Work'} />
               
               <div className="w-0 h-[1px] WorkLine absolute bottom-[-12] left-0 transition-all duration-200 ease-out bg-black group-hover:w-full"></div>
             </a>
-            <a href="#contact" className=" relative group  ">
+            <a href="#contact" className=" relative group h-fit  ">
               
               <div onClick={handleContactClick}><AnimatedTitle text={'Contact'} /></div>
               <div className="w-0 h-[1px] Line absolute bottom-[-12] left-0 transition-all duration-200 ease-out bg-black group-hover:w-full"></div>
